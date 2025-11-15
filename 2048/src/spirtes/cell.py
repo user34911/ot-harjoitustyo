73c51cacb46715130a26ee274bd1f48e57dc7e96
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-11-15 17:47:11 +0200

    Update cell rendering

--- 2048/src/spirtes/cell.py
+++ 2048/src/spirtes/cell.py
@@ -1,13 +1,19 @@
 import pygame
 
 class Cell(pygame.sprite.Sprite):
-    def __init__(self, colour="lightpink", size=50, x=0, y=0):
+    def __init__(self, size=50, x=0, y=0):
         super().__init__()
 
-        self.image = pygame.Surface((size, size))
-        self.image.fill(colour)
+        self.cell_colour = pygame.Color(255, 243, 209)
+        self.border_colour = pygame.Color(138, 138, 138)
 
+        self.image = pygame.Surface((size, size))
+        self.image.fill(self.cell_colour)
+        self.image.set_colorkey(self.cell_colour)
+        
         self.rect = self.image.get_rect()
+        self.border_width = size // 20
+        pygame.draw.rect(self.image, self.border_colour, self.rect, width=self.border_width)
 
         self.rect.x = x
         self.rect.y = y
