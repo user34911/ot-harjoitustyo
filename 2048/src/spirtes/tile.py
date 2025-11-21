6549b6619b09192e74475b175bdc16596cdeb853
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-11-21 20:04:15 +0200

    Add colours and value to tile

--- 2048/src/spirtes/tile.py
+++ 2048/src/spirtes/tile.py
@@ -1,13 +1,25 @@
 import pygame
 
+TILE_COLOURS = {2: pygame.Color(255, 233, 196),
+                4: pygame.Color(255, 220, 161),
+                8: pygame.Color(250, 199, 112),
+                16: pygame.Color(255, 161, 0),
+                32: pygame.Color(207, 132, 58)}
+
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, size, x=0, y=0, colour="orange"):
+    def __init__(self, size, value, x=0, y=0):
         super().__init__()
 
+        self.value = value
+
         self.image = pygame.Surface((size, size))
-        self.image.fill(colour)
+        self._draw_image()
 
         self.rect = self.image.get_rect()
 
         self.rect.x = x
         self.rect.y = y
+    
+    def _draw_image(self):
+        colour = TILE_COLOURS[self.value]
+        self.image.fill(colour)
