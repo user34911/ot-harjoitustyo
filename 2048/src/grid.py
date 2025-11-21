e6c2575765372c2c985b60862821d21238588b34
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-11-21 22:19:43 +0200

    Commented code

--- 2048/src/grid.py
+++ 2048/src/grid.py
@@ -23,7 +23,9 @@ class Grid:
         # Initialise playing grid tiles are on
         for y in range(grid_size):
             for x in range(grid_size):
+                # X pos of cell calulated by x index * size of a cell + starting x
                 normalised_x = x * self.cell_size + self.x
+                # Y pos of cell calulated by y index * size of a cell + starting y
                 normalised_y = y * self.cell_size + self.y
                 self.cells.add(Cell(size=self.cell_size, x=normalised_x, y=normalised_y))
 
@@ -40,17 +42,22 @@ class Grid:
         # Right border
         self.borders.add(Border(width=border_thickness, height=border_length, x=self.x+border_offset, y=self.y))
 
-        # Get 2 random cells and spawn a tile on them
+        # Add 2 tiles to grid
         self._spawn_tile()
         self._spawn_tile()
 
         self.all_sprites.add(self.tiles, self.cells, self.borders)
 
     def _spawn_tile(self):
+        # Pick value for new tile, 75% of picking 2
         value = random.choice([2, 2, 2, 4])
+        # Pick a cell that hasn't got a tile on it by random
         spawn_cell = random.choice([cell for cell in self.cells.sprites() if not cell.tile])
+        # Make a new tile on picked cell's postion
         new_tile = Tile(size=self.cell_size, value=value, x=spawn_cell.rect.x, y=spawn_cell.rect.y)
+        # Set picked cell's status indicating if a cell has a tile on to True
         spawn_cell.tile = True
+        # Add new tile to sprite groups
         self.tiles.add(new_tile)
         self.all_sprites.add(new_tile)
 
@@ -58,16 +65,21 @@ class Grid:
         self.tiles.update()
 
     def move_down(self):
+        # Make a loop that moves tiles down for as long as there are movable tiles
         move_loop = True
-
         while move_loop:
+            # Get list of tiles that can move DOWN
             movable_tiles = self._get_movable_tiles(Direction.DOWN)
+            # Sort tiles to check BOTTOM row first to make sure tiles move and combine correctly
             movable_tiles = sorted(movable_tiles, key=lambda tile: tile.rect.y, reverse=True)
+            # When no movable tiles left break the loop
             if len(movable_tiles) == 0:
                 move_loop = False
 
             for tile in movable_tiles:
+                # Move the tile down one cell
                 tile.rect.move_ip(0, self.cell_size)
+                # Check if tile can combine
                 self._combine_tiles(tile)
 
         self._update_cell_tiles()
@@ -75,16 +87,21 @@ class Grid:
         self._unlock_all_tiles()
 
     def move_up(self):
+        # Make a loop that moves tiles up for as long as there are movable tiles
         move_loop = True
-
         while move_loop:
+            # Get list of tiles that can move UP
             movable_tiles = self._get_movable_tiles(Direction.UP)
+            # Sort tiles to check TOP row first to make sure tiles move and combine correctly
             movable_tiles = sorted(movable_tiles, key=lambda tile: tile.rect.y)
+            # When no movable tiles left break the loop
             if len(movable_tiles) == 0:
                 move_loop = False
 
             for tile in movable_tiles:
+                # Move the tile up one cell
                 tile.rect.move_ip(0, -self.cell_size)
+                # Check if tile can combine
                 self._combine_tiles(tile)
 
         self._update_cell_tiles()
@@ -92,16 +109,21 @@ class Grid:
         self._unlock_all_tiles()
 
     def move_left(self):
+        # Make a loop that moves tiles left for as long as there are movable tiles
         move_loop = True
-
         while move_loop:
+            # Get list of tiles that can move LEFT
             movable_tiles = self._get_movable_tiles(Direction.LEFT)
+            # Sort tiles to check LEFT column first to make sure tiles move and combine correctly
             movable_tiles = sorted(movable_tiles, key=lambda tile: tile.rect.x)
+            # When no movable tiles left break the loop
             if len(movable_tiles) == 0:
                 move_loop = False
 
             for tile in movable_tiles:
+                # Move the tile left one cell
                 tile.rect.move_ip(-self.cell_size, 0)
+                # Check if tile can combine
                 self._combine_tiles(tile)
 
         self._update_cell_tiles()
@@ -109,16 +131,21 @@ class Grid:
         self._unlock_all_tiles()
 
     def move_right(self):
+        # Make a loop that moves tiles right for as long as there are movable tiles
         move_loop = True
-
         while move_loop:
+            # Get list of tiles that can move RIGHT
             movable_tiles = self._get_movable_tiles(Direction.RIGHT)
+            # Sort tiles to check RIGHT column first to make sure tiles move and combine correctly
             movable_tiles = sorted(movable_tiles, key=lambda tile: tile.rect.x, reverse=True)
+            # When no movable tiles left break the loop
             if len(movable_tiles) == 0:
                 move_loop = False
 
             for tile in movable_tiles:
+                # Move the tile right one cell
                 tile.rect.move_ip(self.cell_size, 0)
+                # Check if tile can combine
                 self._combine_tiles(tile)
 
         self._update_cell_tiles()
@@ -126,71 +153,93 @@ class Grid:
         self._unlock_all_tiles()
 
     def _get_movable_tiles(self, direction):
+        # How much tile is moved when testing for collisions
         move_test_value = 10
         movables = []
+
         for tile in self.tiles:
 
             if direction is Direction.DOWN:
+                # Move tile
                 tile.rect.move_ip(0, move_test_value)
-                if self._collisions(tile):
+                # Check for collisions
+                if not self._collisions(tile):
+                    # If no collisions add tile to movables
                     movables.append(tile)
+                # Move tile back to original position
                 tile.rect.move_ip(0, -move_test_value)
 
             elif direction is Direction.UP:
                 tile.rect.move_ip(0, -move_test_value)
-                if self._collisions(tile):
+                if not self._collisions(tile):
                     movables.append(tile)
                 tile.rect.move_ip(0, move_test_value)
 
             elif direction is Direction.LEFT:
                 tile.rect.move_ip(-move_test_value, 0)
-                if self._collisions(tile):
+                if not self._collisions(tile):
                     movables.append(tile)
                 tile.rect.move_ip(move_test_value, 0)
 
             elif direction is Direction.RIGHT:
                 tile.rect.move_ip(move_test_value, 0)
-                if self._collisions(tile):
+                if not self._collisions(tile):
                     movables.append(tile)
                 tile.rect.move_ip(-move_test_value, 0)
 
         return movables
 
     def _collisions(self, tile):
+        # Check if tile collides with grid borders
         border_collisions = pygame.sprite.spritecollide(tile, self.borders, False)
         # Make a test group which doesnt include the tile itself
         test_tiles = pygame.sprite.Group([t for t in self.tiles if t != tile])
-
+        # Check if tile collides with other tiles
         tile_collisions = pygame.sprite.spritecollide(tile, test_tiles, False)
+
         if tile_collisions:
+            # Make a list for corrected tile collisions
             corrected_collisions = []
+            # Iter through tile collisions ignoring tiles that can be combined with
             for collided_tile in tile_collisions:
+                # If tile collides with a tile of different value count it as a collision
                 if collided_tile.value != tile.value:
                     corrected_collisions.append(tile)
 
+                # If either of tiles is locked count it as a collision
                 elif collided_tile.lock is True or tile.lock is True:
                     corrected_collisions.append(tile)
 
+            # If there were no collisions after correction set list to None
             if len(corrected_collisions) < 1:
                 corrected_collisions = None
             tile_collisions = corrected_collisions
 
+        # If tile collided with something return True
         if border_collisions or tile_collisions:
-            return False
-        return True
+            return True
+        return False
 
     def _combine_tiles(self, tile):
+        # If tile is locked do nothing
         if tile.lock is True:
             return
+        # Make a test group which doesnt include the tile itself
         test_tiles = pygame.sprite.Group([t for t in self.tiles if t != tile])
+        # Collision checker let's tiles that can be combined stack on top of each other
+        # Get stacked tile and kill it
         tile_collisions = pygame.sprite.spritecollide(tile, test_tiles, True)
         if tile_collisions:
+            # Make a new tile with double value of current tile
             new_tile = Tile(size=self.cell_size, value=tile.value*2, x=tile.rect.x, y=tile.rect.y, lock=True)
+            # Add new tile to sprite groups
             self.tiles.add(new_tile)
             self.all_sprites.add(new_tile)
+            # Kill the old tile
             tile.kill()
 
     def _update_cell_tiles(self):
+        # Iter through every cell on grid and check if there's a tile on it
         for cell in self.cells:
             tile_on_cell = pygame.sprite.spritecollide(cell, self.tiles, False)
             if tile_on_cell:
