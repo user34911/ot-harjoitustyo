bd2d5dec081ed323ea900121f2a46919a9a4932f
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-12-17 03:29:19 +0200

    Make grid position and size adjust according to resolution

--- 2048/src/game.py
+++ 2048/src/game.py
@@ -17,16 +17,24 @@ class Game:
 
     def _initialise_game(self):
         grid_size = self.options.get(Option.GRID_SIZE)
-        position = self.options.get(Option.POSITION)
-        cell_size = self._calculate_cell_size(grid_size, position)
+        cell_size = self._calculate_cell_size(grid_size)
+        position = self._calculate_position(grid_size, cell_size)
         mode = self.options.get(Option.MODE)
         self._grid = Grid(grid_size, cell_size, position, mode)
         self._loop = GameLoop(self._grid, self.renderer, self.options, EventQueue(), Clock())
         self.renderer.set_grid(self._grid)
 
-    def _calculate_cell_size(self, grid_size, position):
+    def _calculate_cell_size(self, grid_size):
         resolution = self.options.get(Option.RESOLUTION)
-        margin = position[0]
-        total_width = resolution[0] - 2 * margin
-        cell_size = total_width // grid_size
+        margin = max(100, min(resolution[0] // 10, resolution[1] // 10))
+        max_width = resolution[0] - 2 * margin
+        max_height = resolution[1] - 2 * margin
+        cell_size = min(max_width, max_height) // grid_size
         return cell_size
+
+    def _calculate_position(self, grid_size, cell_size):
+        resolution = self.options.get(Option.RESOLUTION)
+        offset = (grid_size * cell_size) // 2
+        x = (resolution[0] // 2) - offset
+        y = (resolution[1] // 2) - offset
+        return (x, y)
--- 2048/src/options.py
+++ 2048/src/options.py
@@ -6,14 +6,12 @@ class Options:
         resolution = get_resolution()
         user = get_user()
         grid_size = 4
-        position = (100, 100)
         mode = Mode.STANDARD
         theme_path = r"src\ui\theme.json"
         state = State.MENU
 
         self._options = {Option.RESOLUTION: resolution,
                          Option.GRID_SIZE: grid_size,
-                         Option.POSITION: position,
                          Option.MODE: mode,
                          Option.THEME_PATH: theme_path,
                          Option.STATE: state,
