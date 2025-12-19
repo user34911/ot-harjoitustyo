3c4444d2434aeaff52402cc59069bef66b6eb6a9
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-12-20 00:23:28 +0200

    Add button to change username

--- 2048/src/ui/main_menu.py
+++ 2048/src/ui/main_menu.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame_gui.elements import UIButton
+from repository.config_repository import get_user
 
 class MainMenu:
     def __init__(self, window_size):
@@ -8,27 +9,38 @@ class MainMenu:
         self.start_button = None
         self.exit_button = None
         self.leaderboard_button = None
+        self.username_button = None
+        self._username = get_user()
     
     def recreate(self, manager):
         button_width = 300
         button_height = 100
 
-        self.start_button = UIButton(pygame.Rect((int(self.window_size[0] / 2) - button_width / 2,
-                                                 int(self.window_size[1] - 500)),
+        self.start_button = UIButton(pygame.Rect((int(self.window_size[0] // 2) - button_width // 2,
+                                                (int(self.window_size[1] // 2) - button_height // 2) - button_height - 50),
                                                 (button_width, button_height),),
                                     "Start",
                                     manager,
                                     object_id='#start_button')
-        self.exit_button = UIButton(pygame.Rect((int(self.window_size[0] / 2) - button_width / 2,
-                                                 int(self.window_size[1] - 200)),
+
+        self.leaderboard_button = UIButton(pygame.Rect((int(self.window_size[0] // 2) - button_width // 2,
+                                                (int(self.window_size[1] // 2) - button_height // 2)),
+                                                (button_width, button_height),),
+                                    "Leaderboards",
+                                    manager,
+                                    object_id='#lb_button')
+
+        self.exit_button = UIButton(pygame.Rect((int(self.window_size[0] // 2) - button_width // 2,
+                                                 (int(self.window_size[1] // 2) - button_height // 2) + button_height + 50),
                                                 (button_width, button_height),),
                                     "Exit",
                                     manager,
                                     object_id='#exit_button')
 
-        self.leaderboard_button = UIButton(pygame.Rect((int(self.window_size[0] / 2) - button_width / 2,
-                                                 int(self.window_size[1] - 350)),
+        button_height = 50
+        self.username_button = UIButton(pygame.Rect((int(self.window_size[0] // 2) - button_width // 2,
+                                                 int(self.window_size[1]) - button_height),
                                                 (button_width, button_height),),
-                                    "Leaderboards",
+                                    f"Player: {self._username}",
                                     manager,
-                                    object_id='#lb_button')
+                                    object_id='#username_button')
